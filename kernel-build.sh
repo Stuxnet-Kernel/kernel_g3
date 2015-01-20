7844f966242eca2722e9d79dcf9f6b8f58d9a9f1
Jean Francis Dominique S. Mabalot <jmabalot@google.com>
2015-01-20 13:55:43 +0800

    build script Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

---
+++ kernel-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+
+echo ""
+echo "****************************************************"
+echo "* Im not responsible of what you do to your device.*"
+echo "* You know the consequences upon changing stuff. *"
+echo "* Its your responsibility upon building my kernel. *"
+echo "****************************************************"
+echo ""
+
+git checkout Stuxnet
+
+make clean
+make mrproper
+export CCACHE=1
+export ARCH=arm
+export CROSS_COMPILE=~/Stuxnet/arm-eabi-4.8/bin/arm-eabi-
+make stuxnet_d855_defconfig
+time make -j8 2>&1 | tee kernel.log
+
+echo ""
+echo "****************************************************"
+echo "* Kernel building successful unless it errors out. *"
+echo "* Check the errors and fix them if you changed it. *"
+echo "* Otherwise flash it and jerk off with your iPhone!*"
+echo "****************************************************"
+echo ""
