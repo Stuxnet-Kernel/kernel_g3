69f8208cb2ab5f81e0e096b7e56758910691f910
Jean Francis Dominique S. Mabalot <jmabalot@google.com>
2015-01-29 21:49:47 +0100

    adjust defconfigs Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

--- kernel-build.sh
+++ kernel-build.sh
@@ -17,7 +17,7 @@ make mrproper
 export CCACHE=1
 export ARCH=arm
 export CROSS_COMPILE=/data1/Gnome/G3/arm-eabi-4.8/bin/arm-eabi-
-make stuxnet_d855_defconfig
+make cyanogenmod_d855_defconfig
 time make -j8 2>&1 | tee kernel.log
 mv kernel.log ../
 
